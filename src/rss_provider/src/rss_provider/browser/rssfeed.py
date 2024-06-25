77943e8e3e903c9e80b1e9393b4f7c253bc75d9d
nileshgulia1 <nileshgulia@gmail.com>
2024-06-26 00:41:19 +0530

    add view class

--- src/rss_provider/src/rss_provider/browser/rssfeed.py
+++ src/rss_provider/src/rss_provider/browser/rssfeed.py
@@ -2,4 +2,10 @@ from Products.Five.browser import BrowserView
 
 
 class rssfeedView(BrowserView):
-    pass
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        req = self.request
+        return self.index()
