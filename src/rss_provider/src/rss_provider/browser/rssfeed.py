6f989d73957665530cd3b27243121351eefa5101
nileshgulia1 <nileshgulia@gmail.com>
2024-06-26 14:28:37 +0530

    initialized browser view

--- src/rss_provider/src/rss_provider/browser/rssfeed.py
+++ src/rss_provider/src/rss_provider/browser/rssfeed.py
@@ -1,11 +1,16 @@
 from Products.Five.browser import BrowserView
 
+from plone.restapi.deserializer import json_body
+
+import json
+
 
 class rssfeedView(BrowserView):
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
+    # def __init__(self, context, request):
+    #     self.context = context
+    #     self.request = request
 
     def __call__(self):
-        req = self.request
-        return self.index()
+        query = json_body(self.request)
+
+        return json.dumps(query)
